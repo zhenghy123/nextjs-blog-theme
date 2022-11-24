631e41e5c8b58768c800129ebe790fa1d5fbc157
zhenghy <1014660095@qq.com>
2022-11-24 09:07:01 +0800

    fix img

--- src/core/player-interactives.js
+++ src/core/player-interactives.js
@@ -1,8 +1,18 @@
 // 互动组件枚举
 export const InteractiveEnums = {
-  单击: {
+  pointClick: {
     beforeTrigger: '/assets/imgs/pointClick/beforeTrigger.png',
     triggering: '/assets/imgs/pointClick/triggering.png',
     afterTrigger: '/assets/imgs/pointClick/afterTrigger.png',
+  },
+  optionsBranch: {
+    beforeTrigger: '/assets/imgs/optionsBranch/beforeTrigger.png',
+    triggering: '/assets/imgs/optionsBranch/triggering.png',
+    afterTrigger: '/assets/imgs/optionsBranch/afterTrigger.png',
+  },
+  combinationClick: {
+    beforeTrigger: '/assets/imgs/combinationClick/beforeTrigger.png',
+    triggering: '/assets/imgs/combinationClick/triggering.png',
+    afterTrigger: '/assets/imgs/combinationClick/afterTrigger.png',
   },
 }
